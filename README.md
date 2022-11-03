0bb7fb13c3b1f499462234ae6c9879278437e0d2
lizeng <lizeng@guanghe.tv>
2022-11-03 22:11:01 +0800

    feat: 初始化项目

---
+++ README.md
@@ -0,0 +1,2 @@
+# source-learning
+vue3、react18、vite、rollup、webpack等源码系统学习项目
---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    /* for example, use global to avoid globals imports (describe, test, expect): */
+    // globals: true,
+    exclude: ["packages/**/*"],
+    coverage: {
+      provider: 'c8'
+    },
+  },
+})
